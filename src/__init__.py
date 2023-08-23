5f99a649e23c55a92ae884c50d2b25f913ac2d79
Andrew Parry <2393265p@student.gla.ac.uk>
2023-08-23 13:44:18 +0100

    wyhy

--- src/__init__.py
+++
@@ -1,5 +0,0 @@
-from conceptqr.models import LM
-from conceptqr.models.generativeQR import GenerativeConceptQR
-import conceptqr.chains.weighting
-import conceptqr.chains.conceptexpansion 
-import conceptqr.chains.conceptextraction 
