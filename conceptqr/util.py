da1ab867dd0471c98e9a6fe362316a9104c600a5
Andrew Parry <2393265p@student.gla.ac.uk>
2023-08-23 14:51:56 +0100

    will this work?

--- conceptqr/util.py
+++ conceptqr/util.py
@@ -5,5 +5,5 @@ def batch_iter(iterable, n=1):
     for ndx in range(0, l, n):
         yield iterable[ndx:min(ndx + n, l)]
 
-def concatenate(*lists):
+def concatentate(lists):
     return list(itertools.chain.from_iterable(lists))
