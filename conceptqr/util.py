c8ccf4eff19cf70c85afd4b3c28f69b9243b062b
Andrew Parry <2393265p@student.gla.ac.uk>
2023-08-23 14:53:29 +0100

    typo

--- conceptqr/util.py
+++ conceptqr/util.py
@@ -5,5 +5,5 @@ def batch_iter(iterable, n=1):
     for ndx in range(0, l, n):
         yield iterable[ndx:min(ndx + n, l)]
 
-def concatentate(lists):
+def concatenate(lists):
     return list(itertools.chain.from_iterable(lists))
