42fd7a01a23ec440764096a53a9866f58b641336
Andrew Parry <2393265p@student.gla.ac.uk>
2023-08-23 14:50:02 +0100

    stop the silly

--- conceptqr/util.py
+++ conceptqr/util.py
@@ -6,4 +6,4 @@ def batch_iter(iterable, n=1):
         yield iterable[ndx:min(ndx + n, l)]
 
 def concatenate(*lists):
-    return itertools.chain.from_iterable(lists)
+    return list(itertools.chain.from_iterable(lists))
