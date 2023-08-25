a6b79eeaf2bba3817b0998ae20ff3d0e9ffd8d5e
Andrew Parry <2393265p@student.gla.ac.uk>
2023-08-25 14:18:09 +0100

    add grid run and beamsearch

--- conceptqr/models/generation.py
+++ conceptqr/models/generation.py
@@ -31,3 +31,17 @@ contrastive = {
     'early_stopping' : True,
     'penalty_alpha' : 0.5,
 }
+
+beam_search ={
+    'min_length' : 20,
+    'max_new_tokens' : 40,
+    'temperature' : 1.2,
+    'do_sample' : False,
+    'top_k' : 50,
+    'top_p' : 0.92,
+    'repetition_penalty' : 2.1,
+    'early_stopping' : True,
+    'num_beams' : 5,
+    'num_return_sequences' : 1,
+    'no_repeat_ngram_size' : 1,
+}
---
+++ eval/run_bm25.py
@@ -0,0 +1,38 @@
+import pyterrier as pt
+if not pt.started():
+    pt.init()
+
+from fire import Fire
+import os 
+import pandas as pd
+import ir_measures
+from ir_measures import *
+
+def main(topic_dir : str, out_dir : str):
+    files = [f for f in os.listdir(topic_dir) if os.path.isfile(os.path.join(topic_dir, f))]
+
+    if not os.path.exists(out_dir):
+        os.makedirs(out_dir, exist_ok=True)
+
+    bm25 = pt.BatchRetrieve.from_dataset(pt.get_dataset("msmarco_passage"), "terrier_stemmed", wmodel="BM25")
+    
+    runs = []
+
+    for f in files:
+        name = os.path.basename(f).strip('.tsv')
+        topics = pd.read_csv(os.path.join(topic_dir, f), sep='\t', index_col=False)
+        rez = bm25.transform(topics)
+
+        rez.to_csv(os.path.join(out_dir, f'{name}_ranking.tsv'), sep='\t', index=False)
+
+        qrels = pt.get_dataset("msmarco_passage").get_qrels(variant='test-2019')
+
+        evaluator = ir_measures.evaluator([nDCG(rel=2)@10, R(rel=2)@1000, P(rel=2)@10, P(rel=2)@100, RR], qrels)
+
+        metrics = evaluator.calc_aggregate(rez)
+        metrics['name'] = name
+        runs.append(metrics)
+    
+    runs = pd.DataFrame.from_records(runs)
+    runs.to_csv(os.path.join(out_dir, 'metrics.tsv'), sep='\t', index=False)
+
