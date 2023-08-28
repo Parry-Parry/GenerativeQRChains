b06205e14303ac25df31ecfdc55f8917df654b78
Andrew Parry <2393265p@student.gla.ac.uk>
2023-08-28 16:20:49 +0100

    reduce search space

--- eval/all_weightings.py
+++ eval/all_weightings.py
@@ -24,13 +24,20 @@ def main(
         name = os.path.basename(file).replace('.tsv', '')
         for prune_k in PRUNE_K:
             for weight in weights:
-                for beta in BETA:
                     for topk in TOPK:
-                        args = main_args
-                        out_path = os.path.join(out_dir, f'{name}_{beta}_{topk}_{prune_k}_{weight}.tsv')
-                        args += f' --mode {weight} --intermediate {file} --beta {beta} --topk {topk} --out_path {out_path}'
-                        if prune_k > 0: args += f' --prune_k {prune_k}'
-                        sp.run(args, shell=True)
+                        if weight == 'fixed' or weight == 'cwprf':
+                            for beta in BETA:
+                                args = main_args
+                                out_path = os.path.join(out_dir, f'{name}_{beta}_{topk}_{prune_k}_{weight}.tsv')
+                                args += f' --mode {weight} --intermediate {file} --beta {beta} --topk {topk} --out_path {out_path}'
+                                if prune_k > 0: args += f' --prune_k {prune_k}'
+                                sp.run(args, shell=True)
+                        else:
+                            args = main_args
+                            out_path = os.path.join(out_dir, f'{name}_{0.0}_{topk}_{prune_k}_{weight}.tsv')
+                            args += f' --mode {weight} --intermediate {file} --topk {topk} --out_path {out_path}'
+                            if prune_k > 0: args += f' --prune_k {prune_k}'
+                            sp.run(args, shell=True)
     
 if __name__ == "__main__":
     Fire(main)
