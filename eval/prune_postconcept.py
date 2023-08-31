c948711d64fcd02d50b91a90d0da3d64caa83589
Andrew Parry <2393265p@student.gla.ac.uk>
2023-08-31 13:36:52 +0100

    agh

--- eval/prune_postconcept.py
+++ eval/prune_postconcept.py
@@ -20,8 +20,8 @@ import torch
 DATASET = 'irds:msmarco-passage/trec-dl-2019/judged'
 INDEX_DATASET = 'msmarco_passage'
 LM_NAME_OR_PATH = 'google/flan-t5-xxl'    
-CONCEPTS = [1, 3, 5, 10]
-TOPK = [5, 10]
+CONCEPTS = [10]
+TOPK = [5, 10, 15]
 
 def concatenate_concepts(inp):
     # group by qid and concatenate expansion_terms over concept columns
